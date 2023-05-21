a25895593802535dedf2c8a9308ffea1b4ed67e2
Link1393 <edsonandree@gmail.com>
2023-05-21 09:21:31 -0500

    Configuración de la conexión de la base de datos.

---
+++ src/main/java/com/example/crud_ejemplo/models/usuarioModel.java
@@ -0,0 +1,69 @@
+
+package com.example.crud_ejemplo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuario")
+public class usuarioModel {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String cedula;
+    private String correo;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCedula() {
+        return this.cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return this.correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+}
