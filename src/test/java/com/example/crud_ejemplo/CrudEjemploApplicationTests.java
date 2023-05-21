ea1e9c60c6ab50ecc5ebe01120c152564800fcdd
Link1393 <edsonandree@gmail.com>
2023-05-21 08:38:00 -0500

    Subiendo el proyecto

---
+++ src/test/java/com/example/crud_ejemplo/CrudEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
