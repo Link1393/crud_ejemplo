212fc58677c7042bb966038b85441c8517d211f0
Link1393 <edsonandree@gmail.com>
2023-05-21 22:10:55 -0500

    Configuración de UsuarioRepository

--- src/main/java/com/example/crud_ejemplo/repositories/UsuarioRepository.java
+++ src/main/java/com/example/crud_ejemplo/repositories/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.example.crud_ejemplo.repositories;
 
-public class UsuarioRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.crud_ejemplo.models.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long>{
     
 }
