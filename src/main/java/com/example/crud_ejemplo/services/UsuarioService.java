d16e1c64d90c9312cbb9b8759be795cf67ad6aa4
Link1393 <edsonandree@gmail.com>
2023-05-21 22:24:19 -0500

    Metodo findByCedula en UsuarioRepository

--- src/main/java/com/example/crud_ejemplo/services/UsuarioService.java
+++ src/main/java/com/example/crud_ejemplo/services/UsuarioService.java
@@ -1,5 +1,26 @@
 package com.example.crud_ejemplo.services;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.crud_ejemplo.models.UsuarioModel;
+import com.example.crud_ejemplo.repositories.UsuarioRepository;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public ArrayList<UsuarioModel> getUsuarios() {
+        return (ArrayList<UsuarioModel>) this.usuarioRepository.findAll();
+    }
+
+    public Optional<UsuarioModel> getUsuarioById(Long id) {
+        return this.usuarioRepository.findById(id);
+    }
+
     
 }
