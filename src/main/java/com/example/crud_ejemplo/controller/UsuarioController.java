6797dc674301ca1ebef0c9502f5266fb722e50d9
Link1393 <edsonandree@gmail.com>
2023-05-22 06:52:03 -0500

    Implementación de metodos en el controlador

--- src/main/java/com/example/crud_ejemplo/controller/UsuarioController.java
+++ src/main/java/com/example/crud_ejemplo/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package com.example.crud_ejemplo.controller;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.crud_ejemplo.models.UsuarioModel;
@@ -26,5 +30,25 @@ public class UsuarioController {
     @PostMapping
     public UsuarioModel saveUsuario(@RequestBody UsuarioModel usuario) {
         return this.usuarioService.saveUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UsuarioModel> getUsuarioById(@PathVariable("id") Long id) {
+        return this.usuarioService.getUsuarioById(id);
+    }
+
+    @GetMapping("/queryCedula")
+    public ArrayList<UsuarioModel> getUsuarioByCedula(@RequestParam("cedula") String cedula) {
+        return this.usuarioService.getUsuarioByCedula(cedula);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteUsuario(@PathVariable("id") Long id) {
+        boolean success = this.usuarioService.deleteUsuario(id);
+        if (success) {
+            return "Se eliminó el usuario con Id " + id;
+        } else {
+            return "No fue posible elimiar el usuario con id " + id;
+        }
     }
 }
